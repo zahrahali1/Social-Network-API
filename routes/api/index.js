517e6356f24e4c27fe1e347683ac9e53f36f3374
zahrahali1 <zahrahali2000@hotmail.com>
2024-07-08 10:56:36 +0100

    made some changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,7 +7,7 @@ const {
     deleteThought,
     addReaction,
     deleteReaction
-} = require('../controllers/thought-controller');
+} = require('../controllers/thoughtController');
 
 router.route('/')
     .get(getAllThoughts)
